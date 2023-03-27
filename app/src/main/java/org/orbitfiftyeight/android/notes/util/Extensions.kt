d09948e8a81fcb3d53b18744638c062ba1d5993a
Agus S. Mubarok <gusrylmubarok@gmail.com>
2023-03-27 18:49:24 +0700

    upload project

--- app/src/main/java/org/orbitfiftyeight/android/notes/util/Extensions.kt
+++
@@ -1,7 +0,0 @@
-package org.orbitfiftyeight.android.notes.util
-
-import androidx.compose.ui.graphics.Color
-
-fun Color.Companion.fromHex(hex: String): Color {
-  return Color(android.graphics.Color.parseColor(hex))
-}
